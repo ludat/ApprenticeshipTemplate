0889cd3c8d825a1f0b8b76f6eaea21628814b04b
Lucas David Traverso <lucas6246@gmail.com>
2016-10-09 02:45:18 -0300

    Exceptions are great

--- tateti/backend/app/controllers/user_token_controller.rb
+++ tateti/backend/app/controllers/user_token_controller.rb
@@ -1,12 +1,20 @@
 class UserTokenController < ApplicationController
+
+  around_action :exception_catcher
+
   def create
-    user = User.find_by(username: params.require(:username), password: params.require(:password))
-    if user.nil?
-      render json: {error: 'Invalid credentials'}, status: :unauthorized
-    else
-      render json: {
-          token: JWT.encode(
-              {user: user.as_json(only: [:id, :username])}, nil, 'none')}, status: :created
+    user = User.login(params.require(:username), params.require(:password))
+    render json: {
+        token: JWT.encode(
+            {user: user.as_json(only: [:id, :username])}, nil, 'none')}, status: :created
+  end
+
+  private
+  def exception_catcher
+    begin
+      yield
+    rescue UnauthorizedException => e
+      render json: {error: e.message}, status: :unauthorized
     end
   end
 end
--- tateti/backend/app/models/user.rb
+++ tateti/backend/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :games
+
+  def self.login(username, password)
+    user = User.find_by(username: username, password: password)
+    raise UnauthorizedException, 'Invalid credentials' if user.nil?
+    user
+  end
 end
